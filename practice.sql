00831acd2fd0b13b671b8d10b37de83e9d360689
NguyenTrongSon1 <65001546+NguyenTrongSon1@users.noreply.github.com>
2020-11-18 12:41:23 +0700

    sql

---
+++ practice.sql
@@ -0,0 +1,16 @@
+CREATE database if not exists school;
+
+Use school;
+
+CREATE table Student(
+    StudenID varchar(10) primary key not null ,
+    Name varchar(20) not null ,
+    Address varchar(50) not null ,
+    Phone varchar(10) check (Phone not like '%[^0-9]%')
+);
+
+Drop table Student;
+
+INSERT INTO Student values
+('B01010','Nguyen Tuan Anh','Ha Noi',0904818238),
+('B10394','Nguyen Hoang Hai','Hai Duong',0494949494);
